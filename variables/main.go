44afc667c1a77f0d803e5f4ae78d315bf98170a9
samin <84340577+HGSChandeepa@users.noreply.github.com>
2024-01-02 01:02:01 +0530

    go.work file added

--- variables/main.go
+++ variables/main.go
@@ -2,5 +2,5 @@ package main //main package is used to build an executable file
 import "fmt" //fmt package is used to format and print data to standard output (screen)
 
 func main() {
-  fmt.Println("Hello World!")
+  fmt.Println("new data");
 }
